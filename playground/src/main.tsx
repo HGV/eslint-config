3812fafd5fd6219121c300a1576bd14f192575e7
Daniel Fischnaller <daniel.fischnaller@hgv.it>
2025-11-26 12:18:34 +0100

    Add testcase on playground

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+const myUrl = "https://www.google.com";
+console.log(myUrl);
+
 const counter = () => {
   const [counter, setCounter] = React.useState(0);
   return <button onClick={() => setCounter(counter + 1)}>{counter}</button>;
